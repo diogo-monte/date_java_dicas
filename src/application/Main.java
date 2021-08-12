ccfefdc3f52faac88afe7b25a281aef9cc504d60
Diogo Monte <diogo-monte@hotmail.com>
2021-08-12 17:06:37 -0300

    Usando SimpleDaterFormat, datas em ms, e calculo para dias corridos

--- src/application/Main.java
+++ src/application/Main.java
@@ -7,13 +7,14 @@ public class Main {
 
 	public static void main(String[] args) {
 		
+//		ABAIXO, ALGUNS FORMATOS PARA COLOCAR A DATA ATUAL: 
+		
 		SimpleDateFormat x = new SimpleDateFormat("dd/MM/yyyy");
 		SimpleDateFormat y = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
 		SimpleDateFormat z = new SimpleDateFormat("HH:mm:ss");
+//		ABAIXO, USAMOS A CLASSE Date PARA COLOCAR O TEMPO DE AGORA:
 		
 		Date agora = new Date();
-		
-		Date futuro = new Date(2021, 12, 25);
 		
 		System.out.println(agora);
 		
@@ -22,7 +23,20 @@ public class Main {
 		System.out.println(y.format(agora));
 		
 		System.out.println(z.format(agora));
-
+	
+//		ABAIXO, UMA DETERMINADA DATA, RETORNA EM MILISSEGUNDOS:
+		System.out.println(Date.UTC(2021, 8, 10, 21, 31, 0));
+		
+//		ABAIXO,A DATA DE HOJE:
+		System.out.println(agora.getYear()+1900 + ", " +((int) (int) agora.getMonth() + (int) 1) + ", " + (int) agora.getDate());
+//		ABAIXO, DUAS VARIAVEIS, A DATA DE HOJE EM MILISSEG. E A UMA QUALQUER, EM MILISSEG.
+		long agora_milis = (Date.UTC(agora.getYear()+1900, ((int) (int) agora.getMonth() + (int) 1), (int) agora.getDate(), 0, 0, 0));
+		
+		long futuro_milis = (Date.UTC(2021, 12, 25, 0, 0, 0));
+		
+		System.out.println((futuro_milis - agora_milis) / (1000 * 60 * 60 * 24)); 
+//		ACIMA PARA CALCULAR OS DIAS CORRIDOS ENTRE HOJE E UMA DATA FUTURA QUALQUER
+//		NO EXEMPLO ACIMA, RETORNOU OS DIAS CORRIDOS ENTRE A DATA DE HOJE E O FERIADO DE NATAL
 	}
 
 }
