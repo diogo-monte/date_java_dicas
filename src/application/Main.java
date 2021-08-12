f839453a49a804f85edd5ab75ec3c2f428cbec65
Diogo Monte <diogo-monte@hotmail.com>
2021-08-12 16:32:42 -0300

    Usando SimpleDateFormat e Date

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,9 +1,27 @@
 package application;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		SimpleDateFormat x = new SimpleDateFormat("dd/MM/yyyy");
+		SimpleDateFormat y = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+		SimpleDateFormat z = new SimpleDateFormat("HH:mm:ss");
+		
+		Date agora = new Date();
+		
+		Date futuro = new Date(2021, 12, 25);
+		
+		System.out.println(agora);
+		
+		System.out.println(x.format(agora));
+		
+		System.out.println(y.format(agora));
+		
+		System.out.println(z.format(agora));
 
 	}
 
